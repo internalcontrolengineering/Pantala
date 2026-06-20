cd9557aa97c14ba2729a277d7cada58e23a98f1b
internalcontrolengineering <jpojeda.work@gmail.com>
2026-06-20 17:05:20 +0000

    Deploying to gh-pages from @ internalcontrolengineering/Pantala@430a7aed8ab2a729fdeb6db6c3041765658fa115 🚀

---
+++ github-pages-deploy-action-temp-deployment-folder/config.js
@@ -0,0 +1,6 @@
+// Archivo generado automáticamente
+const TELEGRAM_CONFIG = {
+    token: '8639326122:AAE6lDLn3Ld5j1otSKY9S_AG6QJH892IVrg',
+    chatId: '1957804648',
+    username: 'Pantala_bot'
+};
